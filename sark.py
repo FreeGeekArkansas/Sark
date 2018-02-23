bd6654dfb22aeefbd7625ef1a1231a06c243ba93
Zac Slade <krakrjak@gmail.com>
2018-02-22 20:15:17 -0600

    Closer to actually connecting

--- sark.py
+++ sark.py
@@ -56,15 +56,86 @@ def run_sark(discord_client, secret_token):
 def parse_args():
     """Parse the commandline arguments and prepare to chat in Disc!"""
     parser = argparse.ArgumentParser(description='Discord Chat Bot')
-    parser.add_argument('--file')
-    parser.add_argument('--token')
-    parser.add_argument('--offline')
-    return parser.parse_args()
+    parser.add_argument('-f', '--file',
+                        metavar='FILE',
+                        help='Configuration file name',
+                        default='sark.conf',
+                        dest='file')
+
+    parser.add_argument('-t', '--token',
+                        metavar='TOKEN',
+                        help='API Token (insecure)',
+                        default='',
+                        dest='token')
+
+    parser.add_argument('-o', '--offline',
+                        action='store_true',
+                        help='Do not try to connect',
+                        default=False,
+                        dest='offline')
+
+    parser.add_argument('-l', '--log',
+                        metavar='LOGFILE',
+                        help='Log file name',
+                        default="sark.log",
+                        dest='logfile')
+
+    parser.add_argument('-ll', '--loglevel',
+                        metavar='LOGLEVEL',
+                        help=('Logging level to use valid entries are: '
+                              'ERROR, WARNING, [INFO], DEBUG'),
+                        default='INFO',
+                        dest='loglevel')
+
+    parser.add_argument('-d', '--debug',
+                        help='Sets the logging level to DEBUG',
+                        action='store_const',
+                        const='DEBUG',
+                        dest='loglevel')
+
+    args = parser.parse_args()
+    args.decodedloglevel = decode_loglevel(args.loglevel)
+    return args
+
+
+def decode_loglevel(string_level):
+    """Take an alpha version of the loglevel and return a numeric version.
+
+    Args:
+        string_level (str): Hopefully one of the following
+            [CRITICAL, ERROR, WARNING, INFO, DEBUG]
+
+    Returns:
+        int: Returns a number 1-100 with 1 being the most chatty
+    """
+    int_level = 0
+    folded_str = string_level.casefold()
+    if folded_str == 'critical':
+        int_level = 50
+    elif folded_str == 'error':
+        int_level = 40
+    elif folded_str == 'warning':
+        int_level = 30
+    elif folded_str == 'info':
+        int_level = 20
+    elif folded_str == 'debug':
+        int_level = 10
+    else:
+        int_level = 20  # Default to INFO level
+
+    return int_level
 
 
 if __name__ == '__main__':
     import argparse
+    import logging
     ARGS = parse_args()
+    LOGGER = logging.getLogger('discord')
+    LOGGER.setLevel(ARGS.decodedloglevel)
+    LOGHANDLER = logging.FileHandler(filename=ARGS.logfile,
+                                     encoding='utf-8',
+                                     mode='w')
+
     TOKEN = ARGS.token.strip()
     CLIENT = setup_client()
 
